09b4d2c7948c7f053deb9667838715453c4080a0
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-11-04 14:52:42 +0100

    Updating for docker containers moanin & timecourse

---
+++ R_moanin/install.sh
@@ -0,0 +1,4 @@
+apt-get install -y wget unzip
+apt-get install -y libgmp-dev
+
+Rscript install_all_dependencies.R
---
+++ R_timecourse_manuscript/install.sh
@@ -0,0 +1,4 @@
+apt-get install -y wget unzip
+apt-get install -y texlive-latex-extra 
+
+Rscript install_all_dependencies.R
